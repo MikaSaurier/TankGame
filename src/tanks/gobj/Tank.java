aed0c540ef4a5a4cf979afa5e4815fadf4b1b658
noilnoil2 <98523510+noilnoil2@users.noreply.github.com>
2022-02-02 16:22:45 +0100

    Just updated

--- src/tanks/gobj/Tank.java
+++ src/tanks/gobj/Tank.java
@@ -8,6 +8,10 @@ public class Tank {
 	String name;
 	int x, y, width, height;
 	Color color, tmpColor;
+	int[] xPoints = {0,0};
+	int[] yPoints = {0,0};
+	int xPoly[] = {150, 250, 325, 375, 450, 275, 100};
+    int yPoly[] = {150, 100, 125, 225, 250, 375, 300};
 	
 	public Tank(String name, int x, int y, int width, int height, Color color) {
 		this.name = name;
@@ -16,10 +20,18 @@ public class Tank {
 		this.width = width;
 		this.height = height;
 		this.color = color;
+		updatePos();
 	}
 	
 	public void shoot() {
 		System.out.println("Tank " + name + " shooting");
+	}
+	
+	private void updatePos() {
+		xPoints[0] = x;
+		xPoints[1] = x+width;
+		yPoints[0] = y;
+		yPoints[1] = y+height;
 	}
 	
 	public String getName() {
@@ -36,6 +48,7 @@ public class Tank {
 
 	public void setX(int x) {
 		this.x = x;
+		updatePos();
 	}
 
 	public int getY() {
@@ -44,6 +57,7 @@ public class Tank {
 
 	public void setY(int y) {
 		this.y = y;
+		updatePos();
 	}
 
 	public Color getColor() {
@@ -58,7 +72,11 @@ public class Tank {
 		tmpColor = g.getColor();
 		g.setColor(color);
 		g.drawString(name, x, y-width/10);
-		g.fillRect(x, y, width, height);
+		//g.fillRect(x, y, width, height);
+		if(xPoints.length > 0 && yPoints.length > 0) {
+			g.drawPolyline(xPoints, yPoints, xPoints.length);
+		}
+		
 		g.setColor(tmpColor);
 	}
 
