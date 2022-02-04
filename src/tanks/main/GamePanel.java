1bd2749d212eb81ea1b10d7cce1575006b6bb025
Joshix <joshix.dev@mailbox.org>
2022-02-04 22:26:39 +0100

    remove stuff

--- src/tanks/main/GamePanel.java
+++ src/tanks/main/GamePanel.java
@@ -20,7 +20,6 @@ public class GamePanel extends JPanel {
 	public static double s = 1;
 	
 	private ArrayList<Renderable> renderables = new ArrayList<>();
-	private ArrayList<Rectangle> bounds = new ArrayList<>();
 	
 	public GamePanel() {
 		setLayout(null);
@@ -61,9 +60,7 @@ public class GamePanel extends JPanel {
 		double delta = (startTime - lastDrawn) / 1_000_000_000.0;
 		move(delta);
 		this.lastDrawn = startTime;
-		
-		ArrayList<Rectangle> newBounds = new ArrayList<>();
-		
+				
         super.paintComponent(g);
                 
         Graphics2D g2 = (Graphics2D) g;
@@ -78,37 +75,17 @@ public class GamePanel extends JPanel {
         g.drawString("Bullets:  " + Var.bullets.size(), 20, 50);
         g.drawString("Score P1: " + Var.p1.getScore(), 20, 75);
         g.drawString("Score P2: " + Var.p2.getScore(), 20, 100);
-        
-        newBounds.add(new Rectangle(10, 10, 100, 200));
-        
-        //Var.FPSCount.tick();
-        
+                   
         for (Renderable toRender : renderables) {
 			toRender.render(g);
-			newBounds.add(toRender.getBounds());
 		}
         
         Bullet[] bullets = Var.bullets.toArray(new Bullet[0]);
 		for (Bullet bullet : bullets) {
 			bullet.render(g);
-			newBounds.add(bullet.getBounds());
 		}
         
-        if (true || Var.didScreenChange) {
-        	// tu immer dies hier, weil rotierte Panzer
-			repaint();
-			Var.didScreenChange = false;
-		} else {
-        	for (Rectangle bound : newBounds) {
-				repaint(bound);
-			}
-    		for (Rectangle bound : this.bounds) {
-				repaint(bound);
-			}
-		}
-		
-		this.bounds = newBounds;
-        
+        repaint();        
 
 		double renderTime = ((double) (System.nanoTime() - startTime)) / 1_000_000_000.0;
 		if (renderTime < Var.sPerFrame) {
@@ -119,23 +96,23 @@ public class GamePanel extends JPanel {
 	
 	private void move(double delta) {
 		if(Var.P1MoveUP) {
-			Var.p1.move(+100 * delta);
+			Var.p1.move(+200 * delta);
 		} if(Var.P1MoveDOWN) {
-			Var.p1.move(-100 * delta);
+			Var.p1.move(-200 * delta);
 		} if(Var.P1MoveLEFT) {
-			Var.p1.rotate(-0.5 * delta);
+			Var.p1.rotate(-delta);
 		} if(Var.P1MoveRIGHT) {
-			Var.p1.rotate(+0.5 * delta);
+			Var.p1.rotate(+delta);
 		}
 		
 		if(Var.P2MoveUP) {
-			Var.p2.move(-100 * delta);
+			Var.p2.move(+200 * delta);
 		} if(Var.P2MoveDOWN) {
-			Var.p2.move(+100 * delta);
+			Var.p2.move(-200 * delta);
 		} if(Var.P2MoveLEFT) {
-			Var.p2.rotate(-0.5 * delta);
+			Var.p2.rotate(-delta);
 		} if(Var.P2MoveRIGHT) {
-			Var.p2.rotate(+0.5 * delta);
+			Var.p2.rotate(+delta);
 		}
 		
 		Bullet[] bullets = Var.bullets.toArray(new Bullet[0]);
